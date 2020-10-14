f355f721791ad8280a101578051a6778d7951b79
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-14 15:21:29 +0200

    finito di base manca solo il fatto dell arrotondamento

--- js/javascript.js
+++ js/javascript.js
@@ -6,11 +6,22 @@ var prezzobiglietto=(percorso * costoBiglietto);
 
 var scontoMinori= prezzobiglietto * 20 /100;
 var prezzoMinori=prezzobiglietto - scontoMinori;
+
+var scontoOver=prezzobiglietto * 40 /100;
+var prezzoOver=prezzobiglietto - scontoOver;
+
+
 if (etàUtente < 18)
 alert(prezzoMinori)
+
+
+else if(etàUtente > 65)
+alert(prezzoOver)
+
 // if (etàUtente <18)
 // alert(prezzobiglietto - scontoMinorenni)
 
 // alert('costo totale '+ prezzobiglietto+' euro');
 
-// document.getElementById('biglietto').innerHTML= 'il prezzo totale è di euro '+ prezzobiglietto;
+document.getElementById('biglietto').innerHTML= 'il prezzo totale è di euro '+ prezzoMinori;
+document.getElementById('biglietto').innerHTML= 'il prezzo totale è di euro '+ prezzoOver;
