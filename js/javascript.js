4ea114e0ca5f3e41279d6f3386dd098f35ba0fcd
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-14 14:32:21 +0200

    primo calcolo del biglietto

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+var percorso=prompt('distanza da percorrere in km');
+var etàutente=prompt('quanti anni hai');
+
+var costoBiglietto= 0.21;
+var prezzobiglietto=(percorso * costoBiglietto)
+alert('costo totale '+ prezzobiglietto+' euro');
