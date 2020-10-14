3c437a934b711103b279d7046d13e40aa5af3dc7
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-14 15:05:39 +0200

    primo sconto fatto solo da strutturare graficamente

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,16 @@
 var percorso=prompt('distanza da percorrere in km');
-var etàutente=prompt('quanti anni hai');
+var etàUtente=prompt('quanti anni hai');
 
 var costoBiglietto= 0.21;
-var prezzobiglietto=(percorso * costoBiglietto)
-alert('costo totale '+ prezzobiglietto+' euro');
+var prezzobiglietto=(percorso * costoBiglietto);
 
-document.getElementById('biglietto').innerHTML= 'il prezzo totale è di euro '+ prezzobiglietto;
+var scontoMinori= prezzobiglietto * 20 /100;
+var prezzoMinori=prezzobiglietto - scontoMinori;
+if (etàUtente < 18)
+alert(prezzoMinori)
+// if (etàUtente <18)
+// alert(prezzobiglietto - scontoMinorenni)
+
+// alert('costo totale '+ prezzobiglietto+' euro');
+
+// document.getElementById('biglietto').innerHTML= 'il prezzo totale è di euro '+ prezzobiglietto;
