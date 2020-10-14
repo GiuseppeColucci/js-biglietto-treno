e1d3930f965395f49c92b39106b4ac93aa508acd
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-14 14:36:37 +0200

    per ridere

--- js/javascript.js
+++ js/javascript.js
@@ -4,3 +4,5 @@ var etàutente=prompt('quanti anni hai');
 var costoBiglietto= 0.21;
 var prezzobiglietto=(percorso * costoBiglietto)
 alert('costo totale '+ prezzobiglietto+' euro');
+
+document.getElementById('biglietto').innerHTML= 'il prezzo totale è di euro '+ prezzobiglietto;
